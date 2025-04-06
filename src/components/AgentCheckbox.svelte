fc044feb6ae87b3d609e145e9d8d0e1b20dd2501
Dhilan Sunanda Bong <dhilansunandabong@gmail.com>
2025-04-05 21:32:09 -0700

    Implemented more team filters

--- src/components/AgentCheckbox.svelte
+++ src/components/AgentCheckbox.svelte
@@ -1,33 +1,49 @@
 <script>
   import { onDestroy } from 'svelte';
   import { tick } from 'svelte';
-  import { get } from 'svelte/store';
 
   export let agent;
   export let checkedAgents;
-  export let onImageClick;
+  export let mustHaveAgents;
+  export let context;
 
   let checked = false;
+  let checked2 = false;
 
   // Subscribe to `checkedAgents` store
   const unsubscribe = checkedAgents.subscribe(agentsSet => {
     checked = agentsSet.has(agent);
   });
 
-  onDestroy(() => {
-    unsubscribe(); // Clean up subscription when the component is destroyed
+  const unsubscribe2 = mustHaveAgents.subscribe(mustHaveSet => {
+    checked2 = mustHaveSet.has(agent);
   });
 
-  function toggleImage() {
-    checked = !checked;
-    checkedAgents.update(agentsSet => {
-      const updatedSet = new Set(agentsSet); // Create a copy to avoid direct mutation
-      if (checked) {
-        updatedSet.add(agent);
+  onDestroy(() => {
+    unsubscribe(); // Clean up subscription when the component is destroyed
+    unsubscribe2(); // Clean up subscription when the component is destroyed
+  });
+
+  function toggleImage(usedSet) {
+    let checkBox = context === 'owned' ? checked : checked2;
+    checkBox = !checkBox; // Toggle the checkbox state
+    usedSet.update(usedSet => {
+      const updatedSet = new Set(usedSet); // Create a copy to avoid direct mutation
+      if (checkBox) {
+        if (context === 'owned') {updatedSet.add(agent);}
+        else {
+          if (usedSet.size < 3) {
+            updatedSet.add(agent); // Add the agent to the set if not already present
+          } else {
+            // Remove the first added agent if the set is full
+            const firstAgent = Array.from(usedSet)[0]; // Get the first agent in the set
+            updatedSet.delete(firstAgent); // Remove the first agent
+            updatedSet.add(agent); // Add the new agent
+          }
+        }
       } else {
         updatedSet.delete(agent);
       }
-      onImageClick(agent, checked);
       return updatedSet; // Return the updated set
     });
     // Force update the UI
@@ -38,29 +54,24 @@
 </script>
 
 <div>
+  {#if context === 'owned'}
   <!-- svelte-ignore a11y-click-events-have-key-events -->
+  <!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
   <img
     src={`Agents/${agent}.png`}
     alt={agent}
     class="agent-image {checked ? 'selected' : ''}"
-    on:click={toggleImage}
+    on:click={() => toggleImage(checkedAgents)}
   />
-  <p style='text-align: center; '>{agent}</p>
+  {:else if context === 'mustHave'}
+  <!-- svelte-ignore a11y-click-events-have-key-events -->
+  <!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
+  <img
+    src={`Agents/${agent}.png`}
+    alt={agent}
+    class="agent-image {checked2 ? 'selected' : ''}"
+    on:click={() => toggleImage(mustHaveAgents)}
+  />
+  {/if}
+  <p>{agent}</p>
 </div>
-
-<style>
-  .agent-image {
-    width: 100px; /* Adjust size as needed */
-    cursor: pointer;
-    border: 3px solid transparent; /* Default border */
-    border-radius: 8px; /* Rounded corners */
-    transition: border-color 0.3s; /* Smooth border color transition */
-  }
-
-  .agent-image.selected {
-    border-color: #007bff; /* Border color when selected */
-  }
-
-  
-
-</style>
--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,70 +1,84 @@
-<script context="module">
-	import Device from 'svelte-device-info'
-</script>
-
-
-	<title>ZZZ Team Finder v1.1</title>
-	<main>
-		<body>
-		<div class='container'>
-			<div class='agent-selector'>
-				<div class="tabs">
-					<button on:click={() => setActiveTab(0)} class:active={$activeTab === 0}>Agents Owned</button>
-					<button on:click={() => setActiveTab(1)} class:active={$activeTab === 1}>Only Show Teams Containing</button>
-				</div>
-				{#if $activeTab === 0}
-					<h2>Select Owned Agents:</h2>
-					{#if dataLoaded}
-					<div class='agent-grid'>
-						{#each Array.from(agents) as agent (agent)}
-							<AgentCheckbox {agent} {checkedAgents} onImageClick={handleImageClick} />
-						{/each}
-					</div>
-						<div class='button-group'>
-							<button on:click={selectAll}>Select All</button>
-							<button on:click={reset}>Reset</button>
-						</div>
-					{:else}
-						<p>Loading agents...</p>
-					{/if}
-				{:else if $activeTab === 1}
-					<div>
-						<h2>Under development...</h2>
-					</div>
-				{/if}
+<title>ZZZ Team Finder v1.6</title>
+<header>
+	<link rel="stylesheet" href="src/components/style.css">
+</header>
+<body>
+	<div class='container'>
+		<div class='agent-selector'>
+			<div class="tabs">
+				<button on:click={() => setActiveTab(0)} class:active={$activeTab === 0}>Agents Owned</button>
+				<button on:click={() => setActiveTab(1)} class:active={$activeTab === 1}>Only Show Teams With...</button>
 			</div>
-		
-			<div class='possible-teams'>
-				<h2>Possible Teams:</h2>
-				<table>
-					<thead>
-						<!-- <tr>
-							<th class='table-header'>Agent 1</th>
-							<th class='table-header'>Agent 2</th>
-							<th class='table-header'>Agent 3</th>
-						</tr> -->
-					</thead>
-					<tbody class="team-table-body">
-						{#each filteredTeams as team}
-							<tr>
-								{#each team.agents as agent}
-									<td class='agent-card'>
-										<img src={`Agents/${agent}.png`} alt={`${agent}`}/>
-										<br>
-										<p>{agent}</p>
-									</td>
-								{/each}
-							</tr>
-						{/each}
-					</tbody>
-				</table>
-			</div>
-
-
 			
+			{#if $activeTab === 0}
+				<!-- <h2>Select Owned Agents:</h2> -->
+				{#if dataLoaded}
+				<div class='agent-grid owned'>
+					{#each Array.from(agents) as agent (agent)}
+						<AgentCheckbox context='owned' {agent} {checkedAgents} {mustHaveAgents} />
+					{/each}
+				</div>
+				<div class='button-group'>
+					<button on:click={()=>selectAll(checkedAgents)}>Select All</button>
+					<button on:click={()=>reset(checkedAgents)}>Deselect All</button>
+				</div>
+				{:else}
+
+					<p>Loading agents...</p>
+
+				{/if}
+			{:else if $activeTab === 1}
+				<!-- <h2>Select Agents You Want in the Team:</h2> -->
+				<div class='agent-grid mustHave'>
+					<!-- Show all agents in the checkedAgents list -->
+					 {#each Array.from($filteredAgents).filter(agent=>$checkedAgents.has(agent)) as agent (agent)}
+					 	<AgentCheckbox context="mustHave" {agent} {checkedAgents} {mustHaveAgents} />
+					 {/each}
+				</div>
+				<div class='button-group'>
+					<button on:click={()=>reset(mustHaveAgents)}>Deselect All</button>
+				</div>
+			{/if}
 		</div>
-	</body>
-	</main>
+	
+		<div class='possible-teams'>
+			<h2><span id="teamCount"></span> Team(s) Found</h2>
+			<table>
+				<thead>
+					<!-- <tr>
+						<th class='table-header'>Agent 1</th>
+						<th class='table-header'>Agent 2</th>
+						<th class='table-header'>Agent 3</th>
+					</tr> -->
+				</thead>
+				<tbody class="team-table-body">
+					{#if filteredTeams.length > 0}
+						{#each filteredTeams as team}
+						<tr>
+							{#each team.agents as agent}
+								<td class='agent-card'>
+									<img src={`Agents/${agent}.png`} alt={`${agent}`}/>
+									<br>
+									<p>{agent}</p>
+								</td>
+							{/each}
+						</tr>
+						{/each}
+					{:else}
+							<div>
+								<h2 style='color:#d29797'>No teams found with the selected agents.</h2>
+							</div>
+					{/if}
+						
+					
+				</tbody>
+			</table>
+		</div>
+
+
+		
+	</div>
+</body>
 	  
 
 <script>
@@ -72,9 +86,8 @@
 	import { writable } from 'svelte/store';
 	import AgentCheckbox from "./AgentCheckbox.svelte";
 	import Papa from 'papaparse';
-	import { append } from "svelte/internal";
-	import { filter } from "d3";
 
+	let loaded = false;
 	let agents = new Set();
 	let checkedAgents = writable(new Set());
 	let agentAttributes = writable(new Map());
@@ -85,7 +98,7 @@
 
 	let searchQuery = '';
 	let filteredAgents = writable(new Set());
-	let teamRestrictions = writable(new Set());
+	let mustHaveAgents = writable(new Set());
 
 	let searchInput;
 		// Active tab index: 0 for "Agents Owned", 1 for "Exclude Agents"
@@ -122,10 +135,10 @@
 					filteredAgents.set(Array.from(agents));
 					dataLoaded = true;
 
-					checkedAgents.set(new Set(agents));
+					checkedAgents.set(new Set(["Anby", "Nicole", "Billy"]));
 				}
 			});
-			const attributeResponse = await fetch('agentAttributes.csv');
+			const attributeResponse = await fetch('agent_attributes.csv');
 			if (!response.ok) {
 				throw new Error(`HTTP error! Status: ${response.status}`);
 			}
@@ -150,305 +163,59 @@
 	}
 
 	onMount(() => {
-		console.log('Fetching data');
-		fetchData();
+		const height = window.innerHeight;
+		const width = window.innerWidth;
+		if (width < 500 || height < 750) {
+			alert("Sorry, but this app is not optimized for smaller screens.")
+		} else{
+			console.log('Fetching data');
+			fetchData();
+			loaded = true;
+		}
 	});
 
-
-	function handleImageClick(agent) {
-		checkedAgents.update(checkedSet => {
-			const updatedSet = new Set(checkedSet);
-			if (updatedSet.has(agent)) {
-				updatedSet.delete(agent);
-			} else {
-				updatedSet.add(agent);
-			}
-			return updatedSet;
-		});
-	}
 	// Function to handle "Select All" button click
-	function selectAll() {
-		checkedAgents.set(new Set(agents));
+	function selectAll(applyToSet) {
+		console.log(applyToSet)
+		applyToSet.set(new Set(agents));
 	}
 
-	// Function to handle "Reset" button click
-	function reset() {
-		checkedAgents.set(new Set());
+	// Function to handle "reset" button click of the "Agents Owned" tab
+	function reset(applyToSet) {
+		applyToSet.set(new Set());
 	}
 
-	function resetFilters() {
-		teamRestrictions.set(new Set()); // Clear team restrictions
+
+
+	function resetMustHave() {
+		mustHave.set(new Set()); // Clear team restrictions
 	}
 
-	// Function to handle search input
-	function handleSearchInput(event) {
-		searchQuery = event.target.value.toLowerCase();
-		filterAgents();
-	}
-
-	// Function to filter agents based on search query
-	function filterAgents() {
-		const searchQueryLower = searchQuery.toLowerCase();
-		const filtered = Array.from(agents).filter(agent =>
-			agent.toLowerCase().startsWith(searchQueryLower)
-		);
-		filteredAgents.set(new Set(filtered));
-		console.log(filteredAgents);
-	}
-
-	function handleFilterChange(agent, isChecked) {
-		teamRestrictions.update(currentSet => {
-			const updatedSet = new Set(currentSet);
-			if (isChecked) {
-				updatedSet.add(agent);
-			} else {
-				updatedSet.delete(agent);
-			}
-			// Log the updated set
-			console.log(Array.from(updatedSet));
-			return updatedSet;
-		});
-	}
-
-	function handleKeyPress(event) {
-		console.log('Key pressed:', event.key);
-		if (event.key === 'Enter') {
-			console.log('Enter key pressed');
-			let topmostAgent = Array.from($filteredAgents)[0];
-			if (topmostAgent) {
-				teamRestrictions.update(currentSet => {
-					const updatedSet = new Set(currentSet);
-					if (updatedSet.has(topmostAgent)) {
-						updatedSet.delete(topmostAgent);
-					} else {
-						updatedSet.add(topmostAgent);
-					}
-					console.log(Array.from(updatedSet));
-					searchQuery = ''; // Clear search query
-					document.querySelector('.filter-agents input[type="text"]').value = ''; // Clear search input field
-					// Reset filtered agents to all agents
-					filteredAgents.set(Array.from(agents));
-					return updatedSet;
-				});
-			}
-
-		} else if (event.key === '`') {
-			console.log('` key pressed');
-			event.preventDefault(); // Prevent the backtick from being entered
-			resetFilters();
-		}
-	}
-
-	// Reactive statement to filter teams when `checkedAgents` changes
+	// For the "Agents Owned" tab
 	$: {
-		let agentsSet = new Set();
+		const agentsSet = $checkedAgents;
+		const mustHaveSet = $mustHaveAgents;
+		// console.log("Checked Agents:", agentsSet);
 		
-		checkedAgents.subscribe(set => {
-			agentsSet = set;
-			console.log("Checked Agents:", agentsSet);
-			// Filter teams based on checkedAgents
-			filteredTeams = teams.filter(team =>
+		// Have mustHave always be a subset of checkedAgents
+		const intersection = new Set([...mustHaveSet].filter(agent => agentsSet.has(agent)));
+		// mustHaveSet.set(intersection);
+		// console.log("Team Restrictions Set:", mustHaveSet);
+		// console.log("Intersection:", intersection);
+		mustHaveAgents.set(intersection);
+
+		filteredTeams = teams
+			.filter(team =>
+				[...mustHaveSet].every(agent => team.agents.includes(agent))
+			)
+			.filter(team =>
 				team.agents.every(agent => agentsSet.has(agent))
 			);
-		});
-
-		let mustHaveSet = new Set();
-
-		teamRestrictions.subscribe(set => {
-			mustHaveSet = set;
-			console.log("Must Have Agents:", mustHaveSet);
-			
-			if (mustHaveSet.size > 0) {
-				// Further filter the previously filteredTeams based on mustHaveSet
-				filteredTeams = filteredTeams.filter(team =>
-					[...mustHaveSet].every(agent => team.agents.includes(agent))
-				);
-			} else {
-				// If mustHaveSet is empty, use the result from the previous filter
-				filteredTeams = teams.filter(team =>
-					team.agents.every(agent => agentsSet.has(agent))
-				);
-			}
-		});
-	}
-
-	// Reactive statement to keep the search input in focus for PC users
-	$: {
-		if (searchInput && !Device.isMobile) {
-			searchInput.focus();
+		if (loaded) {
+			const teamCount = document.getElementById("teamCount");
+			teamCount.innerText = filteredTeams.length;
 		}
 	}
-
 
 </script>
 
-<style>
-	main {
-		display: flex; /* Use Flexbox for centering */
-		justify-content: center; /* Center items horizontally */
-		align-items: center; /* Center items vertically */
-		width: 100%;
-		height: 100%; /* Ensure it takes the full viewport height */
-		margin: 0 auto;
-		overflow-x: hidden;
-		overflow-y: hidden; /* Hide vertical scrollbar */
-	}
-
-	body {
-		background: #1e1e1e;
-	}
-
-	.container {
-		width: 90%; /* Set container to take up 80% of the viewport width */
-		display: grid;
-		justify-items: center; /* Center items horizontally */
-		grid-template-columns: 1fr 1fr; /* Adjust to make possible-teams wider */
-		max-width: 100%;
-		padding: 1em;
-		max-height: 900px;
-	}
-
-	.agent-selector, .possible-teams {
-		padding: 1em;
-		margin: 1em;
-		border: 5px solid #222; /* Optional: Add a border for visual separation */
-		border-radius: 40px; /* Optional: Add rounded corners */
-		box-shadow: 0px 0px 15px #111;
-		color:white;
-		min-width:500px;
-		max-width:600px;
-	}
-
-	.agent-selector {
-		background: #333; /* Optional: Add background color */
-		overflow-y: auto;
-		max-height: 750px;
-		min-height: 750px;
-		max-width: 100%;
-	}
-
-	.possible-teams {
-		background: #444; /* Optional: Add background color */
-
-	}
-
-	.button-group {
-		display: flex; /* Use Flexbox for button alignment */
-		justify-content: center; /* Center buttons horizontally */
-		margin-top: 1em;
-	}
-
-	.possible-teams h2, .possible-teams th {
-		position:sticky;
-		top: 0px;
-		
-	}
-
-	.possible-teams table {
-		justify-items: center;
-		overflow-y: scroll;
-		max-height: 650px;
-		display: block;
-		padding-left: 1em;
-		padding-right: 1em;
-		margin: 0 auto; /* Center the table */
-		width: 100%;
-		border-collapse: collapse;
-	}
-
-
-	.agent-grid {
-		display: grid;
-		grid-template-columns: repeat(5, 1fr); /* 5 columns layout */
-		justify-items: center; /* Center images in their grid cells */
-		gap:1em;
-		row-gap: 0em;
-		margin-right: 0em; /* Space above the grid */
-
-		max-height: 600px;
-		overflow-y: auto; /* Allow scrolling if text is too long */
-	}
-
-	.agent-card {
-		text-align: center;
-	}
-
-	.agent-card img {
-		width: 100px; /* Adjust size as needed */
-		height: auto;
-		border-radius: 8px; /* Optional: Add rounded corners to images */
-		box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1); /* Optional: Add a shadow for better visuals */
-		margin-bottom: 5px; /* Space between image and text */
-	}
-
-	.agent-card p {
-		margin: 0;
-		font-size: 14px; /* Adjust font size as needed */
-	}
-
-
-
-	.table-header {
-		font-weight: bold;
-		text-align: center;
-	}
-
-	.button-group {
-		margin-top: 1em;
-	}
-
-	.tabs {
-		display: flex;
-		justify-content: center;
-		margin-bottom: 1em;
-		width: 100%;
-	}
-
-	button {
-		margin-right: 0.5em;
-		padding: 0.5em 1em;
-		border: none;
-		border-radius: 4px;
-		background-color: #007bff;
-		color: white;
-		cursor: pointer;
-	}
-
-	button:hover {
-		background-color: #0056b3;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-		text-align: center;
-	}
-
-	h2 {
-		text-align: center;
-	}
-
-	th, td {
-		border: 1px solid #ddd;
-		text-align: left;
-	}
-
-	th {
-		background-color: #f4f4f4;
-		padding: 0.5em;
-		font-size: 1.2em;
-	}
-
-	td {
-		padding:1em;
-	}
-
-	.possible-teams table::-webkit-scrollbar {
-		display:none;
-	}
-	.agent-grid::-webkit-scrollbar {
-		display:none;
-	}
-</style>
